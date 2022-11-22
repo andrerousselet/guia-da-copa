1d258cc26dd5335b62964dc2057b1e2119d0af4f
Andre Rousselet <andre098@hotmail.com>
2022-11-22 13:22:04 -0300

    feat(team icon): add TeamIcon component

---
+++ src/components/TeamIcon.jsx
@@ -0,0 +1,11 @@
+import flags from '../utils/flags';
+import './GuideCategory.css';
+
+export default function TeamIcon() {
+  return (
+    <div className='team-icon chosen-team'>
+      <img src={flags.BRA.url} alt="country-flags" />
+      <p>{flags.BRA.name}</p>
+    </div>
+  )
+};
