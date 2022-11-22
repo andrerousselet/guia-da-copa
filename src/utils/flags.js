45b65aa26ddda0911562990b8f023673f5dc2390
Andre Rousselet <andre098@hotmail.com>
2022-11-22 12:42:24 -0300

    docs(flags): add flags object as utils

---
+++ src/utils/flags.js
@@ -0,0 +1,132 @@
+const flags = {
+  ALE: {
+    name: 'ALE',
+    url: "https://s.sde.globo.com/media/organizations/2019/09/03/Alemanha.svg"
+  },
+  ARG: {
+    name: 'ARG',
+    url: "https://s.sde.globo.com/media/organizations/2019/07/15/Argentina.svg"
+  },
+  ARA: {
+    name: 'ARA',
+    url: "https://s.sde.globo.com/media/organizations/2019/09/05/Ar%C3%A1bia_Saudita.svg"
+  },
+  AUS: {
+    name: 'AUS',
+    url: "https://s.sde.globo.com/media/organizations/2019/09/02/Australia.svg"
+  },
+  BRA: {
+    name: 'BRA',
+    url: "https://s.sde.globo.com/media/organizations/2019/07/16/Brasil_rgYHF6Z.svg"
+  },
+  BEL: {
+    name: "BEL",
+    url: "https://s.sde.globo.com/media/organizations/2019/09/15/B%C3%A9lgica.svg"
+  },
+  CAM: {
+    name: "CAM",
+    url: "https://s.sde.globo.com/media/organizations/2019/09/10/Camar%C3%B5es.svg"
+  },
+  CAN: {
+    name: "CAN",
+    url: "https://s.sde.globo.com/media/organizations/2019/07/15/Canad%C3%A1.svg"
+  },
+  CAT: {
+    name: "CAT",
+    url: "https://s.sde.globo.com/media/organizations/2019/09/06/Catar.svg"
+  },
+  COR: {
+    name: "COR",
+    url: "https://s.sde.globo.com/media/organizations/2019/09/01/Coreia_do_Sul.svg"
+  },
+  CRC: {
+    name: "CRC",
+    url: "https://s.sde.globo.com/media/organizations/2019/07/14/Costa_Rica.svg"
+  },
+  CRO: {
+    name: "CRO",
+    url: "https://s.sde.globo.com/media/organizations/2019/09/16/Cro%C3%A1cia.svg"
+  },
+  DIN: {
+    name: "DIN",
+    url: "https://s.sde.globo.com/media/organizations/2019/09/16/Dinamarca.svg"
+  },
+  EQU: {
+    name: "EQU",
+    url: "https://s.sde.globo.com/media/organizations/2019/07/15/Equador.svg"
+  },
+  ESP: {
+    name: "ESP",
+    url: "https://s.sde.globo.com/media/organizations/2019/09/03/Espanha.svg"
+  },
+  EUA: {
+    name: "EUA",
+    url: "https://s.sde.globo.com/media/organizations/2019/07/16/Estados_Unidos.svg"
+  },
+  FRA: {
+    name: "FRA",
+    url: "https://s.sde.globo.com/media/organizations/2019/09/03/Fran%C3%A7a.svg"
+  },
+  GAN: {
+    name: "GAN",
+    url: "https://s.sde.globo.com/media/organizations/2019/09/08/Gana.svg"
+  },
+  HOL: {
+    name: "HOL",
+    url: "https://s.sde.globo.com/media/organizations/2019/09/16/Holanda.svg"
+  },
+  ING: {
+    name: "ING",
+    url: "https://s.sde.globo.com/media/organizations/2019/09/13/Inglaterra.svg"
+  },
+  IRA: {
+    name: "IRA",
+    url: "https://s.sde.globo.com/media/organizations/2019/09/01/Ir%C3%A3.svg"
+  },
+  JAP: {
+    name: "JAP",
+    url: "https://s.sde.globo.com/media/organizations/2019/09/03/Japao.svg"
+  },
+  MAR: {
+    name: "MAR",
+    url: "https://s.sde.globo.com/media/organizations/2019/09/10/Marrocos.svg"
+  },
+  MEX: {
+    name: "MEX",
+    url: "https://s.sde.globo.com/media/organizations/2019/07/15/M%C3%A9xico.svg"
+  },
+  GAL: {
+    name: "GAL",
+    url: "https://s.sde.globo.com/media/organizations/2019/09/17/Pa%C3%ADs_de_Gales.svg"
+  },
+  POL: {
+    name: "POL",
+    url: "https://s.sde.globo.com/media/organizations/2019/09/02/Polonia_Rl0m1A5.svg"
+  },
+  POR: {
+    name: "POR",
+    url: "https://s.sde.globo.com/media/organizations/2019/09/17/Portugal.svg"
+  },
+  SEN: {
+    name: "SEN",
+    url: "https://s.sde.globo.com/media/organizations/2019/09/03/Senegal.svg"
+  },
+  SUI: {
+    name: "SUI",
+    url: "https://s.sde.globo.com/media/organizations/2019/09/15/Su%C3%AD%C3%A7a.svg"
+  },
+  SER: {
+    name: "SER",
+    url: "https://s.sde.globo.com/media/organizations/2019/09/02/Servia.svg"
+  },
+  TUN: {
+    name: "TUN",
+    url: "https://s.sde.globo.com/media/organizations/2018/03/10/tunisia.svg"
+  },
+  URU: {
+    name: "URU",
+    url: "https://s.sde.globo.com/media/organizations/2019/07/16/Uruguai.svg"
+  },
+}
+
+export default flags;
