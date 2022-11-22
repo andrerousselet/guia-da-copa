88345d2cf8fb6375e09724a9cff37894971575bf
Andre Rousselet <andre098@hotmail.com>
2022-11-22 13:21:37 -0300

    feat(guide category): add styled divs (categories) to hold chosen teams

---
+++ src/components/GuideCategory.jsx
@@ -0,0 +1,29 @@
+import './GuideCategory.css';
+import TeamIcon from './TeamIcon';
+
+export default function GuideCategory({ title, src }) {
+  return (
+    <div>
+      <div className='category-top-section'>
+        <span>
+          <img
+            src={src}
+            alt="emoji-img"
+            height="25px"
+            width="25px"
+          />
+        </span>
+        <span>{title}</span>
+      </div>
+      <div className='category-bottom-section'>
+        <div
+          className='team-icon add-btn'
+          onClick={() => console.log('clicked')}
+        >
+          +
+        </div>
+        <TeamIcon />
+      </div>
+    </div>
+  )
+};
