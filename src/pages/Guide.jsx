2b10d1294fddf694ca550f45e7b4e68a99b2af59
Andre Rousselet <andre098@hotmail.com>
2022-11-22 13:19:24 -0300

    feat(guide): add Guide page with style

--- src/pages/Guide.jsx
+++ src/pages/Guide.jsx
@@ -1,10 +1,30 @@
+import GuideCategory from '../components/GuideCategory';
 import Header from '../components/Header';
+import './Guide.css';
+
+const categories = [
+  { title: "Candidato ao Título", src: "https://s3.glbimg.com/v1/AUTH_08fb001c60b847468664307c11fa9dc9/public/2022/guia/sealWinner.svg" },
+  { title: "Pode Surpreender", src: "https://s3.glbimg.com/v1/AUTH_08fb001c60b847468664307c11fa9dc9/public/2022/guia/sealSurprise.svg" },
+  { title: "Corre por Fora", src: "https://s3.glbimg.com/v1/AUTH_08fb001c60b847468664307c11fa9dc9/public/2022/guia/sealCorrePorForaV2.png" },
+  { title: "Vai a Passeio", src: "https://s3.glbimg.com/v1/AUTH_08fb001c60b847468664307c11fa9dc9/public/2022/guia/sealVaiAPasseio.png" }
+]
 
 export default function Guide() {
   return (
     <>
       <Header />
-      Guia da Copa
+      <main className='main-content'>
+        <section className='category-options'>
+          {categories.map((category) => (
+            <div
+              key={category.title}
+              className="category-option"
+            >
+              <GuideCategory title={category.title} src={category.src} />
+            </div>
+          ))}
+        </section>
+      </main>
     </>
   )
 };
