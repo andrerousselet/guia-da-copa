0fb8898ececb8f443b81894fb05f14088a2ba646
Andre Rousselet <andre098@hotmail.com>
2022-11-23 18:15:07 -0300

    refactor(teams modal): use context to render selection of rankedTeams
    pass props to mapped teams (line 38) to set selection when clicking onto each TeamIcon

--- src/components/TeamsModal.jsx
+++ src/components/TeamsModal.jsx
@@ -1,20 +1,49 @@
+import { useContext, useState } from 'react';
 import ReactDOM from 'react-dom';
 import teams from '../utils/teams';
 import TeamIcon from './TeamIcon';
-import '../styles/teamsModal.css'
+import CloseBtn from './CloseBtn';
+import ConfirmBtn from './ConfirmBtn';
+import TeamsContext from '../context/TeamsContext';
+import Overlay from './Overlay';
+import '../styles/teamsModal.css';
 
-export default function TeamsModal({ open, onClose }) {
+export default function TeamsModal({ open, onClose, category }) {
+  const { rankedTeams } = useContext(TeamsContext);
+  const [selection, setSelection] = useState(rankedTeams);
+
   return open && ReactDOM.createPortal(
     <>
-      <div className='overlay-modal' />
+      <Overlay />
       <div className="teams-modal">
         <section className="teams-modal-top">
-          <button type="button" onClick={onClose}>X</button>
+          <span className='modal-title'>Escolher seleções</span>
+          <CloseBtn onClose={onClose} />
+        </section>
+        <section className='teams-modal-center'>
+          <span>CATEGORIA:
+            {' '}
+            <strong>{category.title}</strong>
+          </span>
+          <ConfirmBtn selection={selection} onClose={onClose} />
+        </section>
+        <section className='teams-modal-selected'>
+          {selection.map(({ team }) => (
+            <div key={team.name}>
+              <TeamIcon name={team.name} flag={team.flag} />
+            </div>
+          ))}
         </section>
         <section className="teams-modal-bottom">
-          {Object.values(teams).map((team) => (
+          {teams.map((team) => (
             <div key={team.name}>
-              <TeamIcon name={team.name} flag={team.url} />
+              <TeamIcon
+                name={team.name}
+                flag={team.flag}
+                category={category.id}
+                selection={selection}
+                setSelection={setSelection}
+              />
             </div>
           ))}
         </section>
