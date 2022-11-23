a50543c020cb22f8a3fb59501dc27e3a182e98e9
Andre Rousselet <andre098@hotmail.com>
2022-11-23 12:16:41 -0300

    feat(app.js): wrap routes inside div with className 'app'
    set max-width of app

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import { Routes, Route } from 'react-router-dom';
 import Guide from './pages/Guide';
 import Criteria from './pages/Criteria';
 import Ratings from './pages/Ratings';
+import './styles/app.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Guide />} />
-      <Route path="/ratings" element={<Ratings />} />
-      <Route path="/criteria" element={<Criteria />} />
-    </Routes>
+    <div className='app'>
+      <Routes>
+        <Route path="/" element={<Guide />} />
+        <Route path="/ratings" element={<Ratings />} />
+        <Route path="/criteria" element={<Criteria />} />
+      </Routes>
+    </div>
   );
 }
 
