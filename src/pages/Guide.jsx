0aa224edc60476cf6c9fd937aa53c7c4c2646178
Andre Rousselet <andre098@hotmail.com>
2022-11-23 17:54:42 -0300

    feat(guide page): replace hooks with context

--- src/pages/Guide.jsx
+++ src/pages/Guide.jsx
@@ -1,40 +1,26 @@
-import { useState } from 'react';
 import GuideCategory from '../components/GuideCategory';
 import Header from '../components/Header';
-import TeamsModal from '../components/TeamsModal';
-import useLocalStorage from '../hooks/useLocalStorage';
 import '../styles/guide.css';
 
 const categories = [
-  { id: "favorite", title: "Candidato ao Título", src: "https://s3.glbimg.com/v1/AUTH_08fb001c60b847468664307c11fa9dc9/public/2022/guia/sealWinner.svg" },
-  { id: "contender", title: "Pode Surpreender", src: "https://s3.glbimg.com/v1/AUTH_08fb001c60b847468664307c11fa9dc9/public/2022/guia/sealSurprise.svg" },
-  { id: "outsider", title: "Corre por Fora", src: "https://s3.glbimg.com/v1/AUTH_08fb001c60b847468664307c11fa9dc9/public/2022/guia/sealCorrePorForaV2.png" },
-  { id: "tourist", title: "Vai a Passeio", src: "https://s3.glbimg.com/v1/AUTH_08fb001c60b847468664307c11fa9dc9/public/2022/guia/sealVaiAPasseio.png" }
+  { id: "favorite", title: "Candidato ao título", src: "https://s3.glbimg.com/v1/AUTH_08fb001c60b847468664307c11fa9dc9/public/2022/guia/sealWinner.svg" },
+  { id: "contender", title: "Pode surpreender", src: "https://s3.glbimg.com/v1/AUTH_08fb001c60b847468664307c11fa9dc9/public/2022/guia/sealSurprise.svg" },
+  { id: "outsider", title: "Corre por fora", src: "https://s3.glbimg.com/v1/AUTH_08fb001c60b847468664307c11fa9dc9/public/2022/guia/sealCorrePorForaV2.png" },
+  { id: "tourist", title: "Vai a passeio", src: "https://s3.glbimg.com/v1/AUTH_08fb001c60b847468664307c11fa9dc9/public/2022/guia/sealVaiAPasseio.png" }
 ]
 
 export default function Guide() {
-  const [rankedTeams, setRankedTeams] = useLocalStorage('ranking', []);
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
     <>
       <Header />
       <main className='main-content'>
         <section className='category-options'>
-          <TeamsModal open={isOpen} onClose={() => setIsOpen(false)} />
           {categories.map((category) => (
             <div
               key={category.id}
               className="category-option"
             >
-              <GuideCategory
-                categoryId={category.id}
-                title={category.title}
-                src={category.src}
-                rankedTeams={rankedTeams}
-                setRankedTeams={setRankedTeams}
-                setIsOpen={setIsOpen}
-              />
+              <GuideCategory category={category} />
             </div>
           ))}
         </section>
