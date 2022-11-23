395680b65eab5636cfd20c0dc8bc75fd936be5b1
Andre Rousselet <andre098@hotmail.com>
2022-11-23 18:17:50 -0300

    refactor(teamicon): create function to select each team on click
    (into selection state, from TeamModal)

--- src/components/TeamIcon.jsx
+++ src/components/TeamIcon.jsx
@@ -1,8 +1,18 @@
 import '../styles/guideCategory.css';
+import '../styles/teamIcon.css';
 
-export default function TeamIcon({ name, flag }) {
+export default function TeamIcon({ name, flag, category, selection, setSelection }) {
+
+  const select = () => {
+    if (setSelection) return setSelection([...selection, { category, team: { name, flag } }]);
+    return null;
+  };
+
   return (
-    <div className='team-icon chosen-team'>
+    <div
+      className='team-icon chosen-team'
+      onClick={select}
+    >
       <img src={flag} alt={`${name}-flag`} />
       <p>{name}</p>
     </div>
