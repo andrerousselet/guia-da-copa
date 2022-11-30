47d0f174ab2a6611ce5e16a8fa08d644f791365e
Andre Rousselet <andre098@hotmail.com>
2022-11-30 11:37:50 -0300

    feat(guide page): add buttons to reset and share guide
    reset function implemented

--- src/pages/Guide.jsx
+++ src/pages/Guide.jsx
@@ -1,5 +1,7 @@
+import { useContext } from 'react';
 import GuideCategory from '../components/GuideCategory';
 import Header from '../components/Header';
+import TeamsContext from '../context/TeamsContext';
 import '../styles/guide.css';
 
 const categories = [
@@ -10,6 +12,7 @@ const categories = [
 ]
 
 export default function Guide() {
+  const { setRankedTeams } = useContext(TeamsContext);
   return (
     <>
       <Header />
@@ -23,6 +26,24 @@ export default function Guide() {
               <GuideCategory category={category} />
             </div>
           ))}
+        </section>
+        <section>
+          <div className="guide-btns">
+            <button
+              className="reset-btn"
+              onClick={() => setRankedTeams([])}
+            >
+              RESETAR
+            </button>
+            <button
+              className="share-btn"
+            >
+              <span>COMPARTILHAR</span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-share-fill" viewBox="0 0 16 16">
+                <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z" />
+              </svg>
+            </button>
+          </div>
         </section>
       </main>
     </>
